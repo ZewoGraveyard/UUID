500a862b972d0fa03a2c3093b549de540460c393
Dan Appel <dan.appel00@gmail.com>
2016-03-27 22:55:32 -0700

    Fix urls in package.swift

--- Package.swift
+++ Package.swift
@@ -3,10 +3,10 @@ import PackageDescription
 let package = Package(
     name: "UUID",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/OS", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/Zewo/OS.git", majorVersion: 0, minor: 4)
     ]
 )
 
 #if os(Linux)
-package.dependencies.append(Package.Dependency.Package(url: "https://github.com/PureSwift/CUUID", majorVersion: 1))
+package.dependencies.append(Package.Dependency.Package(url: "https://github.com/PureSwift/CUUID.git", majorVersion: 1))
 #endif
