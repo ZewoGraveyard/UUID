41950260e3361508ed9bc77940a9ccee9a5c50d9
Paulo Faria <paulo.faria.rl@gmail.com>
2016-06-10 10:16:10 -0300

    add .swift-version

--- Tests/UUID/UUIDTests.swift
+++ Tests/UUID/UUIDTests.swift
@@ -1,48 +1,33 @@
-//
-//  UUIDTests.swift
-//  SwiftFoundation
-//
-//  Created by Alsey Coleman Miller on 7/2/15.
-//  Copyright © 2015 PureSwift. All rights reserved.
-//
-
 import XCTest
 @testable import UUID
 
 class UUIDTests: XCTestCase {
 
     func testCreateRandomUUID() {
-
-        // try to create without crashing
-        let uuid = UUID()
-        print(uuid)
-
-        // pass
+        let _ = UUID()
         XCTAssert(true)
     }
 
     func testUUIDString() {
-
         let stringValue = "5BFEB194-68C4-48E8-8F43-3C586364CB6F"
 
-        guard let uuid = UUID(rawValue: stringValue)
-            else { XCTFail("Could not create UUID from " + stringValue); return }
+        guard let uuid = UUID(rawValue: stringValue) else {
+            return XCTFail("Could not create UUID from " + stringValue)
+        }
 
         XCTAssert(uuid.rawValue == stringValue, "UUID is \(uuid), should be \(stringValue)")
     }
 
     func testCreateFromString() {
-
         let stringValue = "5BFEB194-68C4-48E8-8F43-3C586364CB6F"
 
         XCTAssert((UUID(rawValue: stringValue) != nil), "Could not create UUID with string \"\(stringValue)\"")
-
         XCTAssert((UUID(rawValue: "BadInput") == nil), "UUID should not be created")
     }
 }
 
 extension UUIDTests {
-    static var allTests : [(String, UUIDTests -> () throws -> Void)] {
+    static var allTests : [(String, (UUIDTests) -> () throws -> Void)] {
         return [
            ("testCreateRandomUUID", testCreateRandomUUID),
            ("testUUIDString", testUUIDString),
